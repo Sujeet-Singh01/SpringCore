ab3b30311c4f44756331a853017b41ddbea6318a
satendrasinghrw <60536943+satendrasinghrw@users.noreply.github.com>
2024-02-11 02:03:49 +0530

    Constructor Injection

---
+++ src/main/java/com/springboot/springcoredemo/DemoController.java
@@ -0,0 +1,26 @@
+package com.springboot.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    //define private fields for the dependency
+    private Coach myCoach;
+
+    // define a constructor for dependency injection
+    @Autowired
+    public DemoController(Coach theCoach) {
+        this.myCoach = theCoach;
+    }
+
+    @GetMapping("/getdailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+    @GetMapping("/")
+    public String getStr() {
+        return "Hello";
+    }
+}
