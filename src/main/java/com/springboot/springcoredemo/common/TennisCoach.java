53eda78a5869addf17c705c086c86099d6198f3b
satendrasinghrw <60536943+satendrasinghrw@users.noreply.github.com>
2024-02-24 03:54:44 +0530

    BeanScope

--- src/main/java/com/springboot/springcoredemo/common/TennisCoach.java
+++ src/main/java/com/springboot/springcoredemo/common/TennisCoach.java
@@ -1,8 +1,14 @@
 package com.springboot.springcoredemo.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+
+// By default the Scope is Singleton
+
 public class TennisCoach implements Coach{
     public TennisCoach(){
         System.out.println("In constructor: "+ getClass().getSimpleName());
--- src/main/java/com/springboot/springcoredemo/rest/DemoController.java
+++ src/main/java/com/springboot/springcoredemo/rest/DemoController.java
@@ -3,7 +3,7 @@ package com.springboot.springcoredemo.rest;
 import com.springboot.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     //define private fields for the dependency
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // define a constructor for dependency injection
     @Autowired
-    public DemoController(@Qualifier("tennisCoach") Coach theCoach) {
+    public DemoController(@Qualifier("tennisCoach") Coach theCoach,
+                          @Qualifier("tennisCoach") Coach theAnotherCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         this.myCoach = theCoach;
+        this.anotherCoach = theAnotherCoach;
+    }
+// If the scope is singleton(default) then theCoach and theAnotherCoach
+// will point to the same memory location
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing Coach and AnotherCoach: " + (myCoach == anotherCoach);
     }
 
     @GetMapping("/getdailyworkout")
